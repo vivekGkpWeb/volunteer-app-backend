f47ce48b775960b0e809546d8bec3308750ce4bb
Vivek Rao <vivek@gkpwebsolutions.com>
2022-07-03 15:58:15 +0530

    added api routes

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 4000;
+const users = require('./fakedata/users');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('!! Hello World !!');
 })
 
-app.listen(4000,()=>{
-    console.log('Server is running on port 4000');
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+app.get('/users/:userId', (req, res) => {
+    res.send(users.find(user => user.id == req.params.userId));
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
 })
